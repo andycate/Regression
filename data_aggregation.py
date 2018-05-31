7d50ce1273bfbd3bd1bccf598b5f1159cf2a2aab
andycate <and164v@gmail.com>
2018-05-31 16:56:55 -0700

    The outline of the code is written, now it's just a matter of implementing the code

--- data_aggregation.py
+++ data_aggregation.py
@@ -1,20 +1,37 @@
 import numpy as np
 import struct
 
-def load_data():
-    training_images = open("train-images-idx3-ubyte", "rb")
-    training_labels_raw = open("train-labels-idx1-ubyte", "rb")
-    test_images_raw = open("t10k-images-idx3-ubyte", "rb")
-    test_labels_raw = open("t10k-labels-idx1-ubyte", "rb")
+def format_images(data_file):
+    # read data into numpy array
+    # reshape array
+    # change data type to float32
+    # append ones for intercept term
 
-    # create numpy arrays with the correct data
-    training_images = process_training_images(training_images_raw)
-    training_labels = process_training_labels(training_labels_raw)
-    test_images = process_test_images(test_images_raw)
-    test_labels = process_test_labels(test_labels_raw)
+def format_labels(data_file):
+    # read data into array
+    # change data type to float32
+
+def process_data(imgs, lbls):
+    # select all ones and all zeros
+    # append ones to image data for intercept term
+
+def load_training_data():
+    # open the raw data files
+    training_images_raw = open("train-images-idx3-ubyte", "rb")
+    training_labels_raw = open("train-labels-idx1-ubyte", "rb")
+
+    # create numpy arrays with the correct (raw) data
+    training_images = format_images(training_images_raw)
+    training_labels = format_labels(training_labels_raw)
+
+    # process the data, and prepare it for training
+    training_images, training_labels = process_data(training_images, training_labels)
+
+    # return the processed data
+    return training_images, training_labels
 
     # append 
-`   `
+    training_images = np.append(training_images, np.ones((1, num_images)), axis=0)
 
     try:
         magic_num = struct.unpack(">L", training_images.read(4))[0]
@@ -45,3 +62,18 @@ def load_data():
             print("")
     finally:
         training_images.close()
+
+def load_test_data():
+    # open the raw data files
+    test_images_raw = open("t10k-images-idx3-ubyte", "rb")
+    test_labels_raw = open("t10k-labels-idx1-ubyte", "rb")
+
+    # create numpy arrays with the correct (raw) data
+    test_images = format_images(test_images_raw)
+    test_labels = format_labels(test_labels_raw)
+
+    # process the data, and prepare it for training
+    test_images, test_labels = process_data(test_images, test_labels)
+
+    # return the processed data
+    return test_images, test_labels

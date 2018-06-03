37f8193f66699785efb764ea7ce45e3d213172a6
andycate <and164v@gmail.com>
2018-06-02 22:29:17 -0700

    training works

--- loss_function.py
+++ loss_function.py
@@ -1,12 +1,14 @@
 import numpy as np
 import math
 
-def h_of_x(params, x):
-    dot_product = -params.dot(x)
+def h_of_x(theta, x):
+    dot_product = -theta.dot(x)
     return 1 / (np.exp(dot_product) + 1)
 
-def j_of_theta(params, x, labels): # parameters should be of length 785(for this algorithm)
-    y_hat = h_of_x(params, x)
-    print(y_hat)
+def j_of_theta(theta, x, labels): # parameters should be of length 785(for this algorithm)
+    y_hat = h_of_x(theta, x)
     result = labels*np.log(y_hat) + (1-labels)*np.log(1-y_hat)
     return -np.sum(result)
+
+def gradient_j_of_theta(theta, x, labels):
+    return x.dot((h_of_x(theta, x) - labels).reshape(-1, 1))
--- train.py
+++ train.py
@@ -4,5 +4,11 @@ import loss_function as lf
 import math
 
 training_images, training_labels = da.load_training_data()
-theta = np.random.rand(785)/100
-print(lf.j_of_theta(theta, training_images, training_labels))
+theta = np.random.rand(785) / 100
+learning_rate = 0.00003
+iterations = 10000
+
+for i in range(iterations):
+    print(lf.j_of_theta(theta, training_images, training_labels))
+    gradient = lf.gradient_j_of_theta(theta, training_images, training_labels).reshape(-1)
+    theta = theta - (gradient * learning_rate)

9a7718224a9299ac70fbb56805d2d1b86728b60c
andycate <and164v@gmail.com>
2018-06-02 20:53:37 -0700

    There is rounding error, so I'm gonna try doing zero mean and unit variance normalization

--- loss_function.py
+++ loss_function.py
@@ -3,7 +3,7 @@ import math
 
 def h_of_x(params, x):
     dot_product = -params.dot(x)
-    return 1 / (np.exp(dot_product/np.max(-dot_product)) + 1)
+    return 1 / (np.exp(dot_product) + 1)
 
 def j_of_theta(params, x, labels): # parameters should be of length 785(for this algorithm)
     y_hat = h_of_x(params, x)
